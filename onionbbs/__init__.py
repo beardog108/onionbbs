5d1eaaf97ad1ae78899924f8df27fbf2e530a9c6
Kevin Froman <beardog@mailbox.org>
2020-02-04 21:38:07 -0600

    work on chat server

--- onionbbs/__init__.py
+++ onionbbs/__init__.py
@@ -6,9 +6,10 @@ import time
 from threading import Thread
 
 from gevent import pywsgi
+import requests
 
 import settings
-from chatserver import app
+from chatserver import Chat
 
 try:
     os.mkdir(settings.DATA_DIR)
@@ -16,15 +17,29 @@ except FileExistsError:
     pass
 
 
+def get_chat():
+    return requests.get(f'http://127.0.0.1:{CHAT_SERVER_PORT}/getchat/0').text
+
+def show_chat_forever():
+    while True:
+        print(time.time(), get_chat())
+        time.sleep(1)
+
+CHAT_SERVER_PORT = 5000
 USER = "guest"
 
 try:
     if sys.argv[1] == 'daemon':
         try:
-            server = pywsgi.WSGIServer(('127.0.0.1', 5000), app)
+            server = pywsgi.WSGIServer(('127.0.0.1', CHAT_SERVER_PORT), Chat().app)
             server.serve_forever()
         except KeyboardInterrupt:
             pass
 except IndexError:
-    pass
+    Thread(target=show_chat_forever, daemon=True).start()
+    message = ''
+    while message != '-q':
+        message = input(">")
+        requests.post(
+            f'http://127.0.0.1:{CHAT_SERVER_PORT}/send', data=message)
 print("GOODBYE")
--- onionbbs/chatserver.py
+++ onionbbs/chatserver.py
@@ -1,9 +1,39 @@
 from flask import Flask
 from flask import Response
+from flask import Request
+from flask import abort
+from flask import g
 
-app = Flask(__name__)
 
+class Chat:
+    def __init__(self):
+        self.messages = []
+        app = Flask(__name__)
+        self.app = app
+        chat = Chat()
 
-@app.route('/ping')
-def hello():
-    return Response('pong!')
+        @app.before_request
+        def before_req():
+            g.chat = chat
+
+        @app.route('/ping')
+        def hello():
+            return Response('pong!')
+
+        @app.route('/getchat/<offset>')
+        def show_chat(offset):
+            if len(self.messages) == 0:
+                return self.get()
+            try:
+                return self.get()[offset]
+            except IndexError:
+                return abort(500)
+
+        @app.route('/send', methods=['POST'])
+        def add_message():
+            self.messages.append(Request.data)
+            return Response("success")
+    def send(self, msg):
+        self.messages.append(msg)
+    def get(self):
+        return '\n'.join(self.messages)
